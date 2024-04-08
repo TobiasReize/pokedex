2dbe85ab51cc16f9b59fe253966216ebd628c54f
Tobias Reize <reizeto@web.de>
2024-04-08 18:04:48 +0200

    basis structure

---
+++ script.js
@@ -0,0 +1,14 @@
+let currentPokemon;
+
+async function loadPokemon() {
+    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let response = await fetch(url);
+    currentPokemon = await response.json();
+    console.log(currentPokemon);
+    renderPokemonInfo();
+}
+
+
+function renderPokemonInfo() {
+    document.getElementById('content').innerHTML = currentPokemon['name'];
+}
