ac29ed0321d787efcaa70cf0797833e66b32a3bf
Tobias Reize <reizeto@web.de>
2024-04-09 22:15:55 +0200

    edited first card

--- script.js
+++ script.js
@@ -10,5 +10,20 @@ async function loadPokemon() {
 
 
 function renderPokemonInfo() {
-    document.getElementById('pokemon').innerHTML = currentPokemon['name'];
+    let pokemonId = currentPokemon['id'];
+    let pokemonName = currentPokemon['name'];
+    let pokemonImg = currentPokemon['sprites']['other']['dream_world']['front_default'];
+    let pokemonTypes = currentPokemon['types'];
+
+    document.getElementById('pokemon_id').innerHTML = `#${pokemonId}`;
+    document.getElementById('pokemon_name').innerHTML = pokemonName;
+    document.getElementById('pokemon_img').src = pokemonImg;
+
+    document.getElementById('pokemon_type_container').innerHTML = '';
+    for (let i = 0; i < pokemonTypes.length; i++) {
+        const pokemonType = pokemonTypes[i];
+        document.getElementById('pokemon_type_container').innerHTML += /*html*/ `
+            <div class="pokemon_type">${pokemonType['type']['name']}</div>
+        `;
+    }
 }
